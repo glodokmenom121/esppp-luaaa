67c64b7555c3234c03ded2005d58b1b1ab87b832
glodokmenom121 <endogglodok68@gmail.com>
2025-12-30 15:40:22 +0700

    Update patrick.lua

--- patrick.lua
+++ patrick.lua
@@ -172,3 +172,34 @@ UserInputService.InputBegan:Connect(function(input, gp)
         frame.Visible = not frame.Visible
     end
 end)
+
+-- ===== DRAG GUI =====
+local dragging = false
+local dragStart
+local startPos
+
+frame.InputBegan:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 then
+        dragging = true
+        dragStart = input.Position
+        startPos = frame.Position
+    end
+end)
+
+frame.InputEnded:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 then
+        dragging = false
+    end
+end)
+
+UserInputService.InputChanged:Connect(function(input)
+    if dragging and input.UserInputType == Enum.UserInputType.MouseMovement then
+        local delta = input.Position - dragStart
+        frame.Position = UDim2.new(
+            startPos.X.Scale,
+            startPos.X.Offset + delta.X,
+            startPos.Y.Scale,
+            startPos.Y.Offset + delta.Y
+        )
+    end
+end)
